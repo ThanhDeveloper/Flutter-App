4e524b5c2760db22f7f485981e6ea89d775ff78c
Tony, Nguyen Tam Thanh <nguyentt.ext@medgate.ch>
2024-03-14 16:48:39 +0700

    Add quiz app

---
+++ quizz_app/lib/quiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/question_screen.dart';
+import 'package:quizz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
