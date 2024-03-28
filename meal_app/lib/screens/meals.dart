ddf71ce28c8a0d4b6c104369dd9ff1ed30284654
Tony, Nguyen Tam Thanh <nguyentt.ext@medgate.ch>
2024-03-28 16:45:31 +0700

    Update meal app

--- meal_app/lib/screens/meals.dart
+++ meal_app/lib/screens/meals.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/screens/meal_detail.dart';
 import 'package:meal_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
     required this.meals,
+    this.title,
+    required this.onToggleFavorite,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
+
+  void _selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+          onToggleFavorite: onToggleFavorite,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +51,22 @@ class MealsScreen extends StatelessWidget {
 
     if (meals.isNotEmpty) {
       content = ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal: meals[index])
-      );
+          itemCount: meals.length,
+          itemBuilder: (ctx, index) => MealItem(
+                meal: meals[index],
+                onSelecteMeal: (meal) {
+                  _selectMeal(context, meal);
+                },
+              ));
+    }
+
+    if (title == null) {
+      return content;
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
